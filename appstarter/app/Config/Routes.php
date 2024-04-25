244e008a8fd0896f3acdcf834fe676a9544ad426
Dicky Samudra Alamsyah <58102830+dickysamudra09@users.noreply.github.com>
2024-04-25 12:39:54 +0700

    controller product CRUD

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -12,3 +12,7 @@ $routes->get('/Category-product', 'Controller_category_product::index');
 $routes->get('/Product', 'Controller_product::index');
 $routes->get('/list-seller', 'Controller_list_seller::index');
 
+$routes->add('/product/create', 'Controller_product::create');
+$routes->add('/product/(:segment)/edit', 'Controller_product::edit/$1');
+$routes->get('/product/(:segment)/delete', 'Controller_product::delete/$1');
+
--- appstarter/app/Controllers/Controller_list_seller.php
+++ appstarter/app/Controllers/Controller_list_seller.php
@@ -1,11 +1,15 @@
 <?php namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use \App\Models\model_product;
 
 class Controller_list_seller extends BaseController{
 
     public function index(){
         $data['name'] = "Sellers";
+
+        $news = new model_product();
+        $data['newses'] = $news->findAll();
         echo view("Admin/list-seller_view", $data);
     }
 }
